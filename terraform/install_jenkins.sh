713346de0cce93872fbab879f6fe6cb309ed7912
bogdandragosvasile <bogdan.dragos.gm@gmail.com>
2022-04-16 18:56:25 +0300

    updated install_jenkins.sh

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
-sudo yum -y update
+sudo yum -y update && sudo yum remove -y java
 
 echo "Install Java JDK 8"
-yum remove -y java
 sudo amazon-linux-extras install java-openjdk11 -y
+sleep 10s
 
-echo "Install Maven"
-yum install -y maven 
+echo "Install Maven Git Docker"
+sudo yum update -y && sudo yum install -y maven git docker
+sleep 10s
 
-echo "Install git"
-yum install -y git
-
-echo "Install Docker engine"
-yum update -y
-yum install docker -y
-#sudo usermod -a -G docker jenkins
-sudo systemctl start docker
-sudo systemctl enable docker
+echo "Add Jenkins to docker users"
+sudo usermod -a -G docker jenkins && sudo systemctl start docker && sudo systemctl enable docker
+sleep 10s
 
 echo "Install Jenkins"
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo yum install jenkins -y
-sudo usermod -a -G docker jenkins
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo && sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade && sudo yum install jenkins -y && sudo usermod -a -G docker jenkins && sudo systemctl enable jenkins && sudo systemctl start jenkins && sudo systemctl status jenkins
